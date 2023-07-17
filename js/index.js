7e441bc4e6913326876c4b18070c1049808417a4
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-07-17 19:17:18 +0530

    Refactor clean up unused code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,8 @@
-const switchTheme = (event) => {
-  const element = event.target;
-  element.classList.toggle('bg-dark');
-}
-
-const addText = (event) => {
-  const element = event.target;
-  const message = prompt("Enter you message");
-
-  const span = document.createElement('span');
-  span.innerText = message;
-
-  element.appendChild(span);
-};
-
 const getAsciiCode = (event) => {
-  console.log(event)
   const asciiCode = event.key.charCodeAt();
   const codeBox = document.querySelector('#ascii-code');
 
-  codeBox.innerText = event.key + " : " + asciiCode;
+  codeBox.innerText = `${event.key} : ${asciiCode}`;
 };
 
-const main = () => {
-}
-
 window.onkeydown = getAsciiCode;
-window.onload = main;
